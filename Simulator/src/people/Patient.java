2163c28c917bf3bf79b417709e416211729baa4a
unknown <jbeckner3@gatech.edu>
2020-02-03 20:05:30 -0500

    Started work on user interactivity and added getters'setters for patient

--- Simulator/src/people/Patient.java
+++ Simulator/src/people/Patient.java
@@ -7,4 +7,37 @@ public class Patient {
     public Priority priority;
     public Status status;
     public double waitTime;
+
+    public Patient(Priority p, Status S){
+        waitTime = 0;
+        status = s;
+        priority = p;
+    }
+
+    public Priority getPriority(){
+        return priority;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public double getWaitTime() {
+        return waitTime;
+    }
+
+    public Priority setPriority(Priority p){
+        priority = p;
+        return priority;
+    }
+
+    public Status setStatus(Status s) {
+        status = s;
+        return status;
+    }
+
+    public double setWaitTime(double t) {
+        waitTime = t;
+        return waitTime;
+    }
 }
