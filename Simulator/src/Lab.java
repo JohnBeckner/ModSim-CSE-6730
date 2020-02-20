c49fed40fac6afe86167ff9c0dc803cc41154da4
Julia Corona <jcorona@gatech.edu>
2020-02-20 11:10:45 -0500

    Adding Testing Rooms

---
+++ Simulator/src/Lab.java
@@ -0,0 +1,8 @@
+public class Lab extends Room {
+    private static int id;
+
+    public Lab() {
+        id += 1;
+        //capacity = ?;
+    }
+}
---
+++ Simulator/src/Xray.java
@@ -0,0 +1,9 @@
+public class Xray extends Room{
+    private static int id;
+
+    public Xray() {
+        id += 1;
+        //capacity = ?;
+    }
+
+}
